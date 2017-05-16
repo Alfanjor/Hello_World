3abb67701f8170447a38961f565a6257a6c7b945
Alfanjor <xalfx.gm@gmail.com>
2017-05-16 10:12:32 +0200

    My very first commit from IntelliJ

--- src/Hello_world.java
+++ src/Hello_world.java
@@ -5,6 +5,6 @@ public class Hello_world {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("Hello World!");
-        System.out.println("It's a beautiful day! =D");
+        System.out.println("It's a beutifull day! =D");
     }
 }
