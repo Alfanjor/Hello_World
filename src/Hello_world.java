a12df82f5d943ac962f40defbf63387ac375cd49
Alfanjor <xalfx.gm@gmail.com>
2017-05-16 12:18:14 +0200

    What I am doing?!
    (cherry picked from commit cc66176)

--- src/Hello_world.java
+++ src/Hello_world.java
@@ -6,16 +6,8 @@ public class Hello_world {
         // TODO Auto-generated method stub
         System.out.println("Hello World!");
         System.out.println("It's a beautiful day! =D");
-        // Let's make some changes on the new branch just for fun and testing
-        System.out.println("This is an edit for the new branch.");
-        // Making some more changes
-        System.out.println("GIVE ME MOAR!");
-        // I wanna merge to master branch! >.<
-        // What happen?
-        // Ok just deleted a the other branch, now what happen to master???
-        // Idk what I am doing!! >.<
+        // Fix a lot of bullshit!
 
-        // Making some change to master and separate from the Hello-1.0 branch
 
     }
 }
