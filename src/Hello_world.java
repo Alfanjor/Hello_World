cf7da3510663572f3f57f008f5016a3837014b63
Alfanjor <xalfx.gm@gmail.com>
2017-05-16 10:09:28 +0200

    My very first commit from IntelliJ

---
+++ src/Hello_world.java
@@ -0,0 +1,10 @@
+/**
+ * Created by EXTamedina on 16/05/2017.
+ */
+public class Hello_world {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello World!");
+        System.out.println("It's a beautiful day! =D");
+    }
+}
