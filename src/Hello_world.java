c9e05621929d56f1a82eb99f087b02f17b58c349
Alfanjor <xalfx.gm@gmail.com>
2018-07-17 09:13:33 +0200

    Update Hello_world.java

--- src/Hello_world.java
+++ src/Hello_world.java
@@ -3,13 +3,7 @@
  */
 public class Hello_world {
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         System.out.println("Hello World!");
         System.out.println("It's a beautiful day! =D");
-        // Fix a lot of bullshit!
-
-        // Didn't I made some changes here? Where are them?
-
-
     }
 }
